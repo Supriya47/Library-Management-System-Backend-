e4fb07bf31cb69c7dd1ac5c8f86b9ad555bfb5ec
Prasuna <prashunapokharel12345@gmail.com>
2021-05-10 23:11:28 +0545

    Sprint First

--- librarymanagement/settings.py
+++ librarymanagement/settings.py
@@ -77,9 +77,19 @@ WSGI_APPLICATION = 'librarymanagement.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'db',
+        'USER':'root',
+        'PASSWORD':'',
+        'HOST':'localhost',
+        'PORT':'3306',
+        "OPTIONS": {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES', innodb_strict_mode=1",
+            'charset': 'utf8mb4',
+            "autocommit": True,
+        }
+    
+}
 }
 
 
